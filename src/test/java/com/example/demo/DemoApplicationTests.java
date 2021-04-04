751fdce62e0fc7e421bd85f437565998467ebd72
Yauhen Novikau <Yauhen_Novikau@epam.com>
2021-04-04 13:29:06 +0300

    initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertEquals(1, 1);
+    }
+}
